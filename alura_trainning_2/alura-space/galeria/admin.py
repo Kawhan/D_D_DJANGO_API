a77d86c74f3f0019c9f9ccbd4052d036bcb70187
Kawhan <kawhanlaurindo@outlook.com>
2023-06-21 07:50:00 -0300

    Change files to trainning alura api

--- alura_trainning_2/alura-space/galeria/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from galeria.models import Fotografia
-
-@admin.register(Fotografia)
-class ListandoFotografias(admin.ModelAdmin):
-    list_display = ("id", "nome", "legenda")
-    list_display_links = ("id", "nome")
-    search_fields = ("nome",)
-    list_filter = ("categoria",)
