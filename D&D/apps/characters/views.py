7b0d22a801fe06d466e13231ce71415db7dbac4c
Kawhan <kawhanlaurindo@outlook.com>
2023-06-21 07:58:51 -0300

    Commit to 21/06/2023
    Feature add filters to chacaracters characters app
    Feature add app django_filters to apps
    
    Signed-off-by: Kawhan <kawhanlaurindo@outlook.com>

--- D&D/apps/characters/views.py
+++ D&D/apps/characters/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from characters.models import Character
 from characters.serializer import CharacterSerializer
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 class CharactersViewSet(viewsets.ModelViewSet):
@@ -16,3 +18,8 @@ class CharactersViewSet(viewsets.ModelViewSet):
     serializer_class = CharacterSerializer
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend,
+                       filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['raca']
+    search_fields = ['raca', 'nome']
+    filterset_fields = ['raca', 'nome']
--- D&D/setup/settings.py
+++ D&D/setup/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'characters.apps.CharactersConfig',
     'rest_framework',
+    'django_filters'
 ]
 
 MIDDLEWARE = [
